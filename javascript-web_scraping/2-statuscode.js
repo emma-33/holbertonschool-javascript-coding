ab416c0cadc4043332765264a37183ed9ddd9c67
emma-33 <emma.lateyron@orange.fr>
2024-05-13 16:02:10 +0200

    [add] 2-statuscode.js

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const request = require('request');
+const urlPath = process.argv[2];
+request(urlPath, function (err, response, body) {
+    if (err) throw err;
+    console.log('code:', response.statusCode);
+});
