08b3f6974845843dbe4527e6b14963978491861f
emma-33 <emma.lateyron@orange.fr>
2024-05-13 16:03:03 +0200

    [fix] 2-statuscode.js : semistandard

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -2,6 +2,6 @@
 const request = require('request');
 const urlPath = process.argv[2];
 request(urlPath, function (err, response, body) {
-    if (err) throw err;
-    console.log('code:', response.statusCode);
+  if (err) throw err;
+  console.log('code:', response.statusCode);
 });
