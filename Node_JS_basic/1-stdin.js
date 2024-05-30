0b9c41ad2019cd9123529349c61fa35beef669f5
emma-33 <emma.lateyron@orange.fr>
2024-05-30 15:09:30 +0200

    [add] 4-http.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,17 +1,12 @@
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const lineReading = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+process.stdin.on('readable', () => {
+  const input = process.stdin.read();
+  if (input) {
+    process.stdout.write(`Your name is: ${input}`);
+  }
 });
 
-console.log('Welcome to Holberton School, what is your name?\n');
-
-lineReading.on('line', (input) => {
-  console.log(`Your name is: ${input}`);
-});
-
-lineReading.on('close', () => {
-  console.log('This important software is now closing\n');
-  lineReading.close();
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
