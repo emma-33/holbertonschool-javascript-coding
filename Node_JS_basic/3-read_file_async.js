f00006f3a7528b911bc245932bb863a6324633b5
emma-33 <emma.lateyron@orange.fr>
2024-05-30 21:57:30 +0200

    [fix] 3-read_file_async.js

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -8,24 +8,22 @@ function countStudents(file) {
     const lines = data.toString().split('\n');
 
     console.log(`Number of students: ${lines.length - 1}`);
-        
     const fields = {};
-        
+
     for (let i = 1; i < lines.length; i += 1) {
-        const [name, , , field] = lines[i].split(',');
-        
-        if (field) {
-            fields[field] = fields[field] || { count: 0, names: [] };
-            fields[field].count += 1;
-            fields[field].names.push(name);
-        }
+      const [name, , , field] = lines[i].split(',');
+      if (field) {
+        fields[field] = fields[field] || { count: 0, names: [] };
+        fields[field].count += 1;
+        fields[field].names.push(name);
+      }
     }
-        
+
     for (const field in fields) {
-        if (Object.prototype.hasOwnProperty.call(fields, field)) {
-            console.log(`Number of students in ${field}: ${fields[field].count}. List: ${fields[field].names.join(', ')}`);
-        }
-    }     
-});
+      if (Object.prototype.hasOwnProperty.call(fields, field)) {
+        console.log(`Number of students in ${field}: ${fields[field].count}. List: ${fields[field].names.join(', ')}`);
+      }
+    }
+  });
 }
 module.exports = countStudents;
