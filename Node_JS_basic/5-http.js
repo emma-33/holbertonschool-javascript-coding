707de1b1d119418e67f13e993daeb4cae022b788
emma-33 <emma.lateyron@orange.fr>
2024-05-30 16:03:40 +0200

    [add] 5-http.js 6-http_express.js

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const hostname = 'localhost';
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.write('Hello Holberton School!');
+  res.end();
+});
+
+app.listen(port, hostname, () => {});
+
+module.exports = app;
