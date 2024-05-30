72793a3b48d37924cfb53d6e6aa3d7c50e1cd1f0
emma-33 <emma.lateyron@orange.fr>
2024-05-30 19:52:39 +0200

    [add] 7-http.js task 8 [fix] 2-read_file.js

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,15 +1,29 @@
 const fs = require('fs');
-const parse = require('csv-parse');
 
 function countStudents(file) {
-  const data = fs.readFileSync(file);
-
   try {
-      if (data) {
-        ('Number of students:');
-  }     
-    } catch {
-      (new Error('Cannot load the database'));
-}
+    const data = fs.readFileSync(file, 'utf-8');
+    const lines = data.toString().split('\n');
+
+    console.log(`Number of students: ${lines.length - 1}`);
+
+    const fields = {};
+
+    for (let i = 1; i < lines.length; i += 1) {
+      const [name, , , field] = lines[i].split(',');
+
+      if (field) {
+        fields[field] = fields[field] || { count: 0, names: []};
+        fields[field].count += 1;
+        fields[field].names.push(name);
+      }
+    }
+
+    for (const field in fields) {
+      console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`)
+    }
+  } catch {
+    throw new Error('Cannot load the database');
+  }
 }
 module.exports = countStudents;
